d2b896d52eb80c00eea1ad0165974718b37900e1
Daniel <d.wallgren1987@gmail.com>
2021-06-08 11:05:42 +0200

    Refactor rename project name / repository name

--- src/main/java/com/example/stortorget/StortogetApplication.java
+++ src/main/java/com/example/stortorget/StortogetApplication.java
@@ -1,4 +1,4 @@
-package com.example.stortoget;
+package com.example.stortorget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/stortorget/StortogetApplicationTests.java
+++ src/test/java/com/example/stortorget/StortogetApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.stortoget;
+package com.example.stortorget;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

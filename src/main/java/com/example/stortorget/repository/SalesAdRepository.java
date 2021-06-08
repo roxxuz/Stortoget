3db0b3175b8497180d448bd04ea56908871a6277
Daniel <d.wallgren1987@gmail.com>
2021-06-08 12:24:38 +0200

    Find all ads
    Get all ads from databse

--- src/main/java/com/example/stortorget/repository/SalesAdRepository.java
+++ src/main/java/com/example/stortorget/repository/SalesAdRepository.java
@@ -4,8 +4,12 @@ import com.example.stortorget.entity.SalesAd;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SalesAdRepository extends JpaRepository<SalesAd, Long> {
 
     SalesAd findByUserName(String userName);
+
+    List<SalesAd> findAllByOrderByIdDesc();
 }

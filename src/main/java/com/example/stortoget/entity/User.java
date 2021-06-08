d1e2491dec4ecdf0c5f99c8e01d423594179219d
Daniel <d.wallgren1987@gmail.com>
2021-06-08 10:29:14 +0200

    User entity
    fields, setters, getters

--- src/main/java/com/example/stortoget/entity/User.java
+++ src/main/java/com/example/stortoget/entity/User.java
@@ -1,2 +1,92 @@
-package com.example.stortoget.entity;public class User {
+package com.example.stortoget.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="users")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String userName;
+    private String password;
+    private String phone;
+    private String email;
+    private String role;
+    private String img;
+
+    public User() {
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String name) {
+        this.userName = name;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", img='" + img + '\'' +
+                '}';
+    }
 }

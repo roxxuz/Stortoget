e8634592cc9f4ebd27c569d5e0758fae62d3ed44
Daniel <d.wallgren1987@gmail.com>
2021-06-08 10:47:16 +0200

    Added User Repository

---
+++ src/main/java/com/example/stortoget/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.stortoget.repository;
+
+import com.example.stortoget.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUserName(String userName);
+}
+

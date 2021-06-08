cd67a8405f82198a455a165d104cfc639f0141fd
Daniel <d.wallgren1987@gmail.com>
2021-06-08 11:26:10 +0200

    Added SalesAdService

--- src/main/java/com/example/stortorget/service/SalesAdService.java
+++ src/main/java/com/example/stortorget/service/SalesAdService.java
@@ -1,2 +1,14 @@
-package com.example.stortorget.service;public class SalesAdService {
+package com.example.stortorget.service;
+
+import com.example.stortorget.repository.SalesAdRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalesAdService {
+
+    @Autowired
+    private SalesAdRepository salesAdRepository;
+    
+
 }

b8e9765c1b5edca6adcf52a8f7253549fafab2e4
Daniel <d.wallgren1987@gmail.com>
2021-06-08 11:26:39 +0200

    Added UserService
    + methods.

--- src/main/java/com/example/stortorget/service/UserService.java
+++ src/main/java/com/example/stortorget/service/UserService.java
@@ -1,2 +1,36 @@
-package com.example.stortorget.service;public class UserService {
+package com.example.stortorget.service;
+
+import com.example.stortorget.entity.User;
+import com.example.stortorget.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void saveUser(User user){
+
+        String hashedPass = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPass);
+
+        userRepository.save(user);
+    }
+
+    public User getUserByUserName(String userName){
+
+        return userRepository.findByUserName(userName);
+    }
+
+    public void deleteUser(User user){
+
+        userRepository.delete(user);
+    }
+
 }

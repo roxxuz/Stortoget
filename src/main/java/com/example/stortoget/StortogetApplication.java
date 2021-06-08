4aef30939e4c50d818cdc7efd7610fd9a4255adc
Daniel <d.wallgren1987@gmail.com>
2021-06-08 10:14:01 +0200

    init commit

---
+++ src/main/java/com/example/stortoget/StortogetApplication.java
@@ -0,0 +1,13 @@
+package com.example.stortoget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StortogetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StortogetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stortoget/StortogetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stortoget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StortogetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
